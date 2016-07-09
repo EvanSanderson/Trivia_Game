28b58471754ea708c587d021058d8b1f2171da4f
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-09 11:46:58 -0400

    added additional question and hover over function

--- script.js
+++ script.js
@@ -4,34 +4,45 @@ $(document).ready(function() {
   var questions = [
     {question:"<p>What is two plus three?</p>",
     answers: [2,3,5],
-    answerKey: 5},
+    answerKey: 5
+    },
     {question: "What is an elephant?",
     answers: [2, "An animal", "fruit"],
     answerKey: "An animal"
-    }
+  },
+    {question: "What is Shakespeare's favorite food?",
+    answers: ["Thai", "Indian", "BBQ"],
+    answerKey: "Thai"
+  }
   ]
+
 
 var number = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
 
 
-  function loadQuestion() {
-    // var number = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
-    console.log(number);
-    $(".questions").html(questions[number]["question"]);
-    console.log(questions[number]["question"]);
+function loadQuestion() {
+  // var number = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+  console.log(number);
+  $(".questions").html(questions[number]["question"]);
+  console.log(questions[number]["question"]);
 }
-    function loadAnswers() {
-        $("ul").each(function() {
-          for(i=0;i<3;i++){
-            $("<li/>").appendTo(this)
-            $("li").eq(i).html(questions[number].answers[i]);
-        }
-        })
-      }
+
+function loadAnswers() {
+  $("ul").each(function() {
+    for(i=0;i<3;i++){
+      $("<li/>").appendTo(this)
+      $("li").eq(i).html(questions[number].answers[i]);
+    }
+  })
+}
 
     loadQuestion();
     loadAnswers();
 
+    $("li").hover(function() {
+      console.log("hover working");
+      $(this).toggleClass("answerHover");
+    })
 
 
 // HAd a timer on each question, each question was worth 100 points, each second would eliminate a question,, and worth less points.
